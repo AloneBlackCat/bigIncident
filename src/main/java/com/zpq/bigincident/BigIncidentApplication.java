eecc136e9dfa3b5fdc982d53ed5f0384959698b9
zhang <2210439454@qq.com>
2023-12-08 11:17:04 +0800

    初始化项目

---
+++ src/main/java/com/zpq/bigincident/BigIncidentApplication.java
@@ -0,0 +1,13 @@
+package com.zpq.bigincident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigIncidentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigIncidentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zpq/bigincident/BigIncidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpq.bigincident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigIncidentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
